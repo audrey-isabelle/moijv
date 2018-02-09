7d7feec8f9972b1cd90da25c498c80760c742e19
audrey <audrey.stofel@gmail.com>
2018-02-09 10:13:12 +0100

    Add product form and my products page

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -3,14 +3,15 @@
 namespace App\Entity; //namespace racine
 
 
-use Doctrine\ORM\Mapping as ORM;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
  */
-class User
+class User implements UserInterface, \Serializable
 {
     /**
      * @ORM\Id
@@ -63,8 +64,58 @@ class User
      */
     private $products;
     
+    /**
+     * @ORM\Column(type="string")
+     * @var string
+     */
+    private $roles;
+    
     public function __construct() {
        $this->products = new ArrayCollection(); 
+    }
+    
+    public function setRoles($roles)
+    {
+        $this->roles = $roles;
+        return $this;
+    }        
+            
+    
+    public function getSalt()
+    {
+        // you *may* need a real salt depending on your encoder
+        // see section on salt below
+        return null;
+    }
+    
+    public function getRoles()
+    { // nos roles seront stockés sous ce format : "ROLE_USER|ROLE_ADMIN"
+        return \explode('|', $this->roles);
+    }
+    
+    public function eraseCredentials()
+    {
+    }
+    
+    /** @see \Serializable::serialize() */
+    public function serialize()
+    {
+        return serialize(array(
+            $this->id,
+            $this->username,
+            $this->password,
+            
+        ));
+    }
+
+    /** @see \Serializable::unserialize() */
+    public function unserialize($serialized)
+    {
+        list (
+            $this->id,
+            $this->username,
+            $this->password,
+        ) = unserialize($serialized);
     }
     
     public function getId() {
--- src/Repository/ProductRepository.php
+++ src/Repository/ProductRepository.php
@@ -4,6 +4,7 @@ namespace App\Repository;
 
 use App\Entity\Product;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\ORM\Query\Expr\Join;
 use Symfony\Bridge\Doctrine\RegistryInterface;
 
 class ProductRepository extends ServiceEntityRepository
@@ -22,11 +23,14 @@ class ProductRepository extends ServiceEntityRepository
     }
     
     public function findByTagWithTags($tag) {
-        // SELECT p.*, t.* FROM product p LEFT JOIN product_tag ON product_tag.product_id
-        // = product.id LEFT JOIN tag t ON tag.id = product_tag.tag_id
-        // WHERE tag.name = "qqch"
+        // SELECT p.*, t.* FROM product p
+        // LEFT JOIN product_tag ON product_tag.product_id = product.id
+        // LEFT JOIN tag t ON tag.id = product_tag.tag_id
+        // WHERE tag.id = 25
         return $this->createQueryBuilder('p')
                 ->lefJoin('p.tags', 't')
+                ->leftJoin(Product::class, 'p2', Join::WITH, 'p2.id = p.id')
+                ->leftJoin('p2.tags', 't2')
                 ->addSelect('t')
                 ->where('t.id = :id')
                 ->setParameter(':id', $tag->getId())
